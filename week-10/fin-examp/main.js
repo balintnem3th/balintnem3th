58bf033314466d96ed5b81b3642f04149fc463b0
Németh Bálint <balint.nem3th@gmail.com>
2018-03-26 01:58:32 +0200

    add more examples

--- week-10/fin-examp/main.js
+++ week-10/fin-examp/main.js
@@ -13,3 +13,10 @@ function displayMessage()
         document.getElementById('display2').innerHTML =   error.message;	
     }
 }
+
+function assert(value, description) {
+    var result = value ? "pass" : "fail";
+    console.log(result + ' - ' +  description); 
+};
+assert (1===1, 'testing if 1=1');  
+assert (1===2, 'testing if 1=1');
